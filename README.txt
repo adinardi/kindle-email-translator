63b104223545c060576a41e050f8f02ccffa0cc8
Angelo DiNardi <angelo@dinardi.name>
2012-02-06 00:25:05 -0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+To run this expects a "users.txt" file in the root dir in the format:
+
+    incomingUsername:myusername@free.kindle.com
+    incomingUsername2:another@free.kindle.com
---
+++ mailrelay/mailrelay.go
@@ -0,0 +1,95 @@
+package mailrelay
+
+import (
+    // "fmt"
+    "mail"
+    "http"
+    "path"
+    "appengine"
+    "bytes"
+    amail "appengine/mail"
+    "mime/multipart"
+    "strings"
+    "io/ioutil"
+)
+
+func init() {
+        http.HandleFunc("/_ah/mail/", incomingMail)
+}
+
+func incomingMail(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+
+    // var b bytes.Buffer
+    // if _, err := b.ReadFrom(r.Body); err != nil {
+        // c.Errorf("Error reading body: %v", err)
+        // return
+    // }
+    // c.Infof("Received mail: %v", b.String())
+
+    _, address := path.Split(r.URL.Path)
+
+    var userFileContent []byte
+    userFileContent, _ = ioutil.ReadFile("users.txt")
+
+    var userFileString string
+    userFileString = bytes.NewBuffer(userFileContent).String()
+    users := strings.Split(userFileString, "\n")
+    // c.Infof("Users %v", users)
+    var validUser bool = false
+    var destinationAddress string
+
+    for iter := 0; iter < len(users); iter++ {
+        if items := strings.Split(users[iter], ":"); items[0] + "@kemailtranslator.appspotmail.com" == address {
+            validUser = true
+            destinationAddress = items[1]
+        }
+    }
+
+    if validUser == false {
+        c.Errorf("Invalid user: %v", address)
+        return
+    }
+
+    msg, err := mail.ReadMessage(r.Body)
+    if err != nil {
+        c.Errorf("ERROR PROCESSING BODY: %v", err)
+    }
+
+    var body bytes.Buffer
+
+    contentType := msg.Header.Get("Content-Type")
+    items := strings.Split(contentType, ";")
+
+    if items[0] == "text/plain" {
+        body.ReadFrom(msg.Body)
+    } else {
+        bsplit := strings.Split(items[1], "=")
+        boundary := bsplit[1]
+
+        reader := multipart.NewReader(msg.Body, boundary)
+        part, _ := reader.NextPart()
+
+        body.ReadFrom(part)
+    }
+
+    subject := msg.Header.Get("Subject")
+
+    attachment := amail.Attachment{
+        Name: subject + ".txt",
+        Data: body.Bytes(),
+    }
+
+    email := &amail.Message{
+        Sender:  "angelo@dinardi.name",
+        To:      []string{destinationAddress},
+        Subject: subject,
+        Body:    "attached",
+        Attachments: []amail.Attachment{attachment},
+    }
+
+    if err := amail.Send(c, email); err != nil {
+        c.Errorf("Alas, my user, the email failed to sendeth: %v", err)
+    }
+}
+
